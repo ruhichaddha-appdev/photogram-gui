e1d40e6bbd35fc84b9ab89322060d35f7d0ab26d
ruhichaddha <rchaddha@uchicago.edu>
2022-05-13 00:56:33 +0000

    more photo progress

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   get("/users/:path_username", { :controller => "users", :action => "show"})
 
-  
+  get("/photos", { :controller => "photos", :action => "index"})
 
 end
