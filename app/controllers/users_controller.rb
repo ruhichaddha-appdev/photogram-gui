062ef3dece4d4986754cc7b96303b4e564d6738a
ruhichaddha <rchaddha@uchicago.edu>
2022-05-13 00:45:00 +0000

    progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,18 @@ class UsersController < ApplicationController
     render({ :template => "user_templates/index.html.erb"})
   end
 
+  def show
+    #Parameters: {"path_username"=>"anisa"}
+    url_username = params.fetch("path_username")
+    matching_usernames = User.where({ :username => url_username })
+    @the_user = matching_usernames.at(0)
+    if @the_user == nil
+      redirect_to("/")
+    else
+      render({ :template => "user_templates/show.html.erb"})
+    end
+
+  end
+
 
 end
